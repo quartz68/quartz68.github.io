201a6f259786a199bd1516fcdeb20414ec0ca150
Quartz68 <wangzimeng2004@gmail.com>
2024-12-16 19:27:01 +0000

    Align "My Project" title to left

--- src/components/sections/projects/projects.tsx
+++ src/components/sections/projects/projects.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ProjectCard from './project-card';
 
-import Reveal from '@/components/reveal';
-
 import MotionWrap from '@/components/motion-wrap';
 
 import { project } from '@/app/source';
+import TextReveal from '@/components/motion/text-reveal';
 
 function Projects() {
   const projects = [...project.getPages()].sort(
@@ -17,17 +16,9 @@ function Projects() {
   return (
     <MotionWrap className="w-full py-16 lg:py-24" id="projects">
       <div className="space-y-4 px-4 md:space-y-6 md:px-6 lg:space-y-10">
-        <div className="flex w-full flex-col items-center justify-center text-center lg:flex-row lg:justify-between lg:text-left">
-          <div className="flex flex-col items-start">
-            <Reveal>
-              <h2 className="text-4xl font-bold sm:text-5xl md:text-5xl">
-                My Projects
-              </h2>
-            </Reveal>
-          </div>
-          <p className="mt-4 hidden text-gray-500 dark:text-gray-400 lg:mt-0 lg:block lg:w-[35%]">
-          </p>
-        </div>
+        <h2 className="text-4xl font-bold sm:text-5xl md:text-5xl">
+          <TextReveal>My Projects</TextReveal>
+        </h2>
         <div className="mt-6 grid w-full grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
           {projects.map((project, index) => (
             <ProjectCard
